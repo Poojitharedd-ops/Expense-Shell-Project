008d742bfb283d40d260b8774223364a1da24c6a
poojitha <poojithadspr@gmail.com>
2024-08-28 19:58:28 +0530

    my shell  scripting project for db server

--- Database.sh
+++ Database.sh
@@ -17,6 +17,15 @@ CHECK_ROOT(){
         exit 1
     fi
 }
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT

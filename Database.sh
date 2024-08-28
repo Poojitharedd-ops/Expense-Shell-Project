61eefee6d888eaad8931383f0582aff908cd7796
poojitha <poojithadspr@gmail.com>
2024-08-28 20:07:54 +0530

    my shell  scripting project for db server2

--- Database.sh
+++ Database.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-LOGS_FOLDER="var/log/expense"
-SCRIPT_NAME="$(echo $0 |cut -d "." -f1)
-TIMESTAMP = $(date+%Y-%m-%d-%H-%M-%S )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP"
-mkdir -p LOGS_FOLDER
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -17,6 +19,7 @@ CHECK_ROOT(){
         exit 1
     fi
 }
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -26,21 +29,26 @@ VALIDATE(){
         echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
+
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
+
 dnf install mysql-server -y &>>$LOG_FILE
-VALIDATE $? "mysql service installed or not?"
+VALIDATE $? "Installing MySQL Server"
+
 systemctl enable mysqld &>>$LOG_FILE
-VALIDATE $? "Enabled mysql server"
+VALIDATE $? "Enabled MySQL Server"
+
 systemctl start mysqld &>>$LOG_FILE
-VALIDATE $? "started mysql server"
-mysql -h mysql.poojitha.online -u root -pExpenseApp@1 -e 'show databases';
-if [$? -ne 0]
+VALIDATE $? "Started MySQL server"
+
+mysql -h mysql.daws81s.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+if [ $? -ne 0 ]
 then
-echo " mysql setup is not done, setting up now" &>>$LOG_FILE
-mysql_secure_installation --set-root-pass ExpenseApp@1
-VALIDATE $? "set up root password"
+    echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting UP root password"
 else
- echo -e "MySQL root password is already setup" | tee -a $LOG_FILE
- fi
+    echo -e "MySQL root password is already setup...$Y SKIPPING $N" | tee -a $LOG_FILE
+fi
